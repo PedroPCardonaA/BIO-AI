b6ee177be78955daa1a11e996e594b8ef1fa0f67
Pedro Cardona <pedropca@stud.ntnu.no>
2025-02-06 15:00:49 +0100

    Generate test for population generation

--- project_2/src/main.rs
+++ project_2/src/main.rs
@@ -1,7 +1,36 @@
+use rand::{seq::SliceRandom, Rng};
+use structs::instance::Instance;
+
 mod structs;
 mod utils;
 
 fn main() {
     let instance = utils::parse_data::parse_data("src/data/train/train_0.json");
-    println!("{:?}", instance);
+    let population = generate_population(10, &instance);
+    println!("{:?}", population);
+}
+
+
+fn generate_population(population_size: usize, instance: &Instance) -> Vec<Vec<Vec<usize>>> {
+    let mut population = Vec::new();
+    let patient_count = instance.patients.len();
+    let nurse_count = instance.nurses.len();
+    let mut rng = rand::rng();
+    
+    for _ in 0..population_size {
+        let mut patients: Vec<usize> = (1..(patient_count + 1)).collect();
+        patients.shuffle(&mut rng);
+
+        let mut solution = vec![Vec::new(); nurse_count];
+
+        // Randomly distribute patients to nurses
+        for &patient in &patients {
+            let nurse_index = rng.random_range(0..nurse_count);
+            solution[nurse_index].push(patient);
+        }
+
+        population.push(solution);
+    }
+    
+    population
 }
